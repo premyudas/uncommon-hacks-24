98671372780a8c88c520e4be5cd59d12091e3b47
thorthongthai2 <thor.18706@gmail.com>
2024-03-23 17:23:55 -0500

    auth0 integrated

--- social-publishing-platform/src/App.js
+++ social-publishing-platform/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import RichTextEditor from './components/RichTextEditor';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
+    
     <div className="App">
+      <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
       <RichTextEditor />
     </div>
   );
